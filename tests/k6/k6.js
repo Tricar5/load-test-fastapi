dcc25dd82c6233a36c4c257e5cd1473940c6426f
aakazachinskii <aakazachinskii@domclick.ru>
2023-11-07 12:48:29 +0300

    - added report generation for k6 - fix with .env

--- tests/k6/k6.js
+++ tests/k6/k6.js
@@ -1,6 +1,10 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+// This will export to HTML as filename "result.html" AND also stdout using the text summary
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
 const data =  JSON.parse(open('../body.json'));
 
 export const options = {
@@ -33,4 +37,13 @@ export default function () {
   };
     const response = http.request('POST', url, JSON.stringify(data), params);
     check(response, {'status is 200': (r) => r.status === 200});
+}
+
+
+
+export function handleSummary(data) {
+  return {
+    "tests/result.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
 }
